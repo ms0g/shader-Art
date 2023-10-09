76ac1fb77913435e531542d1f11b7ea2478f1231
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-10-10 01:25:07 +0300

    feat(shaders): added files

---
+++ 0/shaders/main.frag.glsl
@@ -0,0 +1,40 @@
+
+varying vec2 vUvs;
+
+uniform vec2 resolution;
+uniform float aspectRatio;
+uniform float time;
+
+vec3 palette(float t) {
+  vec3 a = vec3(0.5, 0.5, 0.5);
+  vec3 b = vec3(0.5, 0.5, 0.5);
+  vec3 c = vec3(1.0, 1.0, 1.0);
+  vec3 d = vec3(0.263, 0.416, 0.557);
+
+  return a + b * cos(6.28 * (c * t + d));
+}
+
+void main() {
+  vec2 uv = vUvs - 0.5;
+  uv.x *= aspectRatio;
+  
+  vec2 uv0 = uv;
+  vec3 finalColor = vec3(0.0);
+
+  for (float i = 0.0; i < 4.0; i++) {
+    uv = fract(uv * 1.5) - 0.5;
+  
+    float d = length(uv) * exp(-length(uv0));
+  
+    vec3 col = palette(length(uv0) + i*0.4 + time*0.4);
+    
+    d = sin(d*8. + time)/8.;
+    d = abs(d);
+
+    d = pow(0.01 / d, 1.2);
+
+    finalColor += col * d;
+  } 
+
+  gl_FragColor = vec4(finalColor, 1.0);
+}
---
+++ 1/main.js
@@ -0,0 +1,75 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.136';
+
+
+class ShaderArt {
+  constructor() {
+  }
+
+  async initialize() {
+    this.renderer = new THREE.WebGLRenderer();
+    document.body.appendChild(this.renderer.domElement);
+
+    window.addEventListener('resize', () => {
+      this.onWindowResize_();
+    }, false);
+
+    this.scene_ = new THREE.Scene();
+
+    this.camera_ = new THREE.OrthographicCamera(0, 1, 1, 0, 0.1, 1000);
+    this.camera_.position.set(0, 0, 1);
+
+    this.clock_ = new THREE.Clock();
+
+    await this.setup();
+
+    this.onWindowResize_();
+    this.animate();
+  }
+
+  async setup() {
+    const vs = await fetch('./shaders/main.vert.glsl');
+    const fs = await fetch('./shaders/main.frag.glsl');
+
+    const loader = new THREE.TextureLoader();
+    const tex = loader.load('./textures/sincity00.jpeg');
+
+    const material = new THREE.ShaderMaterial({
+      uniforms: {
+        diffuse1: { value: tex },
+        time: { value: 0.0 },
+      },
+      vertexShader: await vs.text(),
+      fragmentShader: await fs.text()
+    });
+
+    this.material_ = material;
+
+    const geometry = new THREE.PlaneGeometry(1, 1);
+    const plane = new THREE.Mesh(geometry, material);
+    plane.position.set(0.5, 0.5, 0);
+    this.scene_.add(plane);
+
+  }
+
+  onWindowResize_() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  animate() {
+    requestAnimationFrame((t) => {
+      this.renderer.render(this.scene_, this.camera_);
+      this.animate();
+    
+    });
+    this.material_.uniforms.time.value = this.clock_.getElapsedTime();
+
+  }
+}
+
+
+let APP_ = null;
+
+window.addEventListener('DOMContentLoaded', async () => {
+  APP_ = new ShaderArt();
+  await APP_.initialize();
+});
