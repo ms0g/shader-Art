11f44192ece1bcecaa57027abe3761bba3a58cdc
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-10-11 02:55:08 +0300

    refactor(glsl): changed inverseLerp and remap params order

--- 1/shaders/main.frag.glsl
+++ 1/shaders/main.frag.glsl
@@ -3,20 +3,20 @@ varying vec2 vUvs;
 uniform sampler2D texture1;
 uniform float time;
 
-float inverseLerp(float v, float minValue, float maxValue) {
+float inverseLerp(float minValue, float maxValue, float v) {
   return (v - minValue) / (maxValue - minValue);
 }
 
-float remap(float v, float inMin, float inMax, float outMin, float outMax) {
-  float t = inverseLerp(v, inMin, inMax);
+float remap(float inMin, float inMax, float outMin, float outMax, float v) {
+  float t = inverseLerp(inMin, inMax, v);
   return mix(outMin, outMax, t);
 }
 
 void main() {
   vec3 colour = vec3(0.0);
 
-  float t1 = remap(sin(vUvs.y * 400.0 + time * 10.0), -1.0, 1.0, 0.9, 1.0);
-  float t2 = remap(sin(vUvs.y * 50.0 - time * 2.0), -1.0, 1.0, 0.9, 1.0);
+  float t1 = remap(-1.0, 1.0, 0.9, 1.0, sin(vUvs.y * 400.0 + time * 10.0));
+  float t2 = remap(-1.0, 1.0, 0.9, 1.0, sin(vUvs.y * 50.0 - time * 2.0));
 
   colour = texture2D(texture1, vUvs).xyz * t1 * t2;
 
